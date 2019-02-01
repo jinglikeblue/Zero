d4e93e2129f3c4808ac89811bf38d41bbfbde697
jing <122300890@qq.com>
2019-02-01 11:43:55 +0800

    代码优化

--- UnityProject/Zero/Assets/Zero/Editor/EditorWins/LinkerXMLEditorWin.cs
+++ UnityProject/Zero/Assets/Zero/Editor/EditorWins/LinkerXMLEditorWin.cs
@@ -52,16 +52,37 @@ namespace Zero.Edit
     {
         string _dir;
 
-        public class NodeVO
+        /// <summary>
+        /// 命名空间节点
+        /// </summary>
+        public class NamespaceNodeVO
         {
-            public string assemblyName;
+            public string name;
             public List<string> typeNameList = new List<string>();
-            public List<string> namespaceNameList = new List<string>();
+
+            public NamespaceNodeVO(string name)
+            {
+                this.name = name;
+            }
         }
 
-        List<NodeVO> _nodeList;
+        /// <summary>
+        /// 动态库节点
+        /// </summary>
+        public class AssemblyNodeVO
+        {
+            public string name;
+            public List<NamespaceNodeVO> nsNodeList = new List<NamespaceNodeVO>();
 
-        public List<NodeVO> NodeList
+            public AssemblyNodeVO(string name)
+            {
+                this.name = name;
+            }
+        }
+
+        List<AssemblyNodeVO> _nodeList;
+
+        public List<AssemblyNodeVO> NodeList
         {
             get { return _nodeList; }
         }
@@ -79,15 +100,15 @@ namespace Zero.Edit
         public event Action<CreateLinkXMLCommand> onCreated;
 
         public CreateLinkXMLCommand(string dir)
-        {            
+        {
             _dir = dir;
         }
 
         public void Excute()
         {
-            _nodeList = new List<NodeVO>();
+            _nodeList = new List<AssemblyNodeVO>();
             var files = Directory.GetFiles(_dir, "*.dll", SearchOption.AllDirectories);
-            for(int i = 0; i < files.Length; i++)
+            for (int i = 0; i < files.Length; i++)
             {
                 var file = files[i];
                 var vo = CreateNodeVO(file);
@@ -99,15 +120,13 @@ namespace Zero.Edit
 
             _linkXMLString = CreateXML(_nodeList);
 
-            if(null != onCreated)
+            if (null != onCreated)
             {
                 onCreated.Invoke(this);
             }
-
-            
         }
 
-        string CreateXML(List<NodeVO> nodeList)
+        string CreateXML(List<AssemblyNodeVO> assemblyNodeList)
         {
             //创建xml实例；
             XmlDocument xmlDoc = new XmlDocument();
@@ -116,51 +135,83 @@ namespace Zero.Edit
             XmlElement root = xmlDoc.CreateElement("linker");
             xmlDoc.AppendChild(root);
 
-            foreach(var node in nodeList)
+            foreach (var assemblyNode in assemblyNodeList)
             {
                 //创建Assembly节点
                 XmlElement assembly = xmlDoc.CreateElement("assembly");
-                assembly.SetAttribute("fullname", node.assemblyName);
+                assembly.SetAttribute("fullname", assemblyNode.name);
 
-                foreach(var nsName in node.namespaceNameList)
+                foreach (var nsNode in assemblyNode.nsNodeList)
                 {
                     XmlElement ns = xmlDoc.CreateElement("namespace");
-                    ns.SetAttribute("fullname", nsName);
+                    ns.SetAttribute("fullname", nsNode.name);
                     ns.SetAttribute("preserve", "all");
                     assembly.AppendChild(ns);
                 }
 
                 root.AppendChild(assembly);
             }
-            
+
             return xmlDoc.InnerXml;
         }
 
-        NodeVO CreateNodeVO(string dllFile)
+        AssemblyNodeVO CreateNodeVO(string dllFile)
         {
-            NodeVO vo = new NodeVO();
-
-            vo.assemblyName = Path.GetFileNameWithoutExtension(dllFile);
-
-            var assembly = Assembly.LoadFile(dllFile);            
+            var dllBytes = File.ReadAllBytes(dllFile);
+            var assembly = Assembly.Load(dllBytes);
+            
+            Type[] typeList = null;
             try
             {
-                var typeList = assembly.GetTypes();
-                foreach (var type in typeList)
-                {
-                    vo.typeNameList.Add(type.Name);
-
-                    if (false == vo.namespaceNameList.Contains(type.Namespace))
-                    {
-                        vo.namespaceNameList.Add(type.Namespace);
-                    }
-                }
+                typeList = assembly.GetTypes();
             }
-            catch
+            catch(Exception e)
             {
-                Debug.LogErrorFormat("无法处理的DLL：{0}", dllFile);
-                vo = null;
+                Debug.LogErrorFormat("无法处理的Assembly:{0}", dllFile);
             }
+
+            if(null == typeList)
+            {
+                return null;
+            }
+
+            AssemblyNodeVO vo = new AssemblyNodeVO(Path.GetFileNameWithoutExtension(dllFile));
+            Dictionary<string, NamespaceNodeVO> _nsDic = new Dictionary<string, NamespaceNodeVO>();
+
+            foreach (var type in typeList)
+            {
+                string nsName = null;
+                string typeName = null;
+                try
+                {
+                    nsName = type.Namespace;
+                    typeName = type.Name;
+                }
+                catch
+                {
+                    Debug.LogErrorFormat("无法处理的Type:{0}", type);
+                    nsName = null;
+                    typeName = null;
+                }
+
+                if(null == nsName || null == typeName)
+                {
+                    continue;
+                }
+
+                if (false == _nsDic.ContainsKey(type.Namespace))
+                {
+                    _nsDic.Add(type.Namespace, new NamespaceNodeVO(type.Namespace));
+                }
+
+                _nsDic[type.Namespace].typeNameList.Add(type.Name);
+            }
+
+            foreach (var entry in _nsDic)
+            {
+                vo.nsNodeList.Add(entry.Value);
+            }
+
             return vo;
         }
     }
