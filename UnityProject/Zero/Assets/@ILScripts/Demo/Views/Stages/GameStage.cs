2ffae05cf412faf41f650e50d626af56e79b1461
Jing <122300890@qq.com>
2019-04-07 15:01:32 +0800

    添加打印

--- UnityProject/Zero/Assets/@ILScripts/Demo/Views/Stages/GameStage.cs
+++ UnityProject/Zero/Assets/@ILScripts/Demo/Views/Stages/GameStage.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using Zero;
 using IL.Zero;
+using System;
 
 namespace IL.Demo
 {
@@ -24,12 +25,23 @@ namespace IL.Demo
         {
             var ac = ResMgr.Ins.Load<AudioClip>("hot_res/audios.ab", "click");
             AudioPlayer.Ins.PlayEffect(ac);
-            //以同步方式创建Block
-            ViewFactory.CreateAsync<Block>("hot_res/prefabs/stages/gamestage.ab","Block", _blocks);            
-            
             //以异步方式创建Block
-            //AView.CreateAsync<Block>("Block", "hot_res/prefabs/stages/gamestage.ab", this, _blocks, null, OnCreated);
+            ViewFactory.CreateAsync<Block>("hot_res/prefabs/stages/gamestage.ab", "Block", _blocks, null, OnCreatedBlock, OnProgressBlock, OnLoadedBlock);
+        }
 
+        private void OnProgressBlock(float progress)
+        {
+            Log.I("方块资源加载进度:{0}", progress);
+        }
+
+        private void OnLoadedBlock(UnityEngine.Object obj)
+        {
+            Log.I("方块资源加载完成:{0}", obj.ToString());
+        }
+
+        private void OnCreatedBlock(Block obj)
+        {
+            Log.I("方块视图创建完成:{0}", obj.ToString());
         }
     }
 }
