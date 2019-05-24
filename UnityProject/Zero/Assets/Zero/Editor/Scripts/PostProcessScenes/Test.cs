49d2088628fb4d48684f233b18b86539b796b9a4
Jing <122300890@qq.com>
2019-05-25 01:01:48 +0800

    修改DLL打包Editor

---
+++ UnityProject/Zero/Assets/Zero/Editor/Scripts/PostProcessScenes/Test.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class Test
+{
+    [PostProcessScene(0)]
+    public static void OnPost()
+    {
+        Debug.Log("PostProcessSceneAttribute");
+    }
+}
