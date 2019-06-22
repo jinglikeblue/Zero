b19cda04b253e2a1284d7493212bcab424c3334a
Jing <122300890@qq.com>
2019-06-22 16:27:41 +0800

    新的Build前和Build后的事件捕获

---
+++ UnityProject/Zero/Assets/Zero/Editor/Scripts/PreprocessBuilds/PreprocessBuildMsg.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class PreprocessBuildMsg : IPreprocessBuildWithReport
+{
+    public int callbackOrder { get { return int.MinValue; } }
+
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        Debug.Log("Build Will Start");
+    }
+}
