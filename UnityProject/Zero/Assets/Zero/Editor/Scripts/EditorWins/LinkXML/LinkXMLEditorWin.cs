e11b06651abbae828bee4360e287b32a5a7cde0a
Jing <122300890@qq.com>
2019-10-17 01:28:59 +0800

    link.xml创建工具重构

--- UnityProject/Zero/Assets/Zero/Editor/Scripts/EditorWins/LinkXML/LinkXMLEditorWin.cs
+++ UnityProject/Zero/Assets/Zero/Editor/Scripts/EditorWins/LinkXML/LinkXMLEditorWin.cs
@@ -1,119 +1,63 @@
 ﻿using Jing;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities;
+using Sirenix.Utilities.Editor;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
-using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 
 namespace Zero.Edit
 {
-    class LinkXMLEditorWin : AEditorWin
+    class LinkXMLEditorWin : OdinEditorWindow
     {
         /// <summary>
         /// 打开窗口
         /// </summary>
         public static void Open()
         {
-            var win = EditorWindow.GetWindow<LinkXMLEditorWin>("Useless Assets Find", true);
-            win.minSize = new Vector2(800, 700);
-            win.maxSize = new Vector2(1000, 700);
-            win.Show();
-        }
-
-        [SerializeField]
-        TreeViewState _tvs;
-
-        LinkAssemblyTreeView _latv;
-
-        SearchField _searchField;
-
-        string _selectDir;
-        List<CreateLinkXMLCommand.AssemblyNodeVO> _nodeList;
+            var win = GetWindow<LinkXMLEditorWin>("link.xml 工具", true);
+            win.position = GUIHelper.GetEditorWindowRect().AlignCenter(800, 600);
+        }              
+        
+        [LabelText("Includes"), ListDrawerSettings(ShowPaging = false, IsReadOnly = true)]
+        public List<CreateLinkXMLCommand.AssemblyNodeVO> nodeList;
         CreateLinkXMLCommand _cmd;
 
-        private void OnEnable()
+        [LabelText("选择DLL所在目录"),Button(size:ButtonSizes.Medium), PropertyOrder(-1)]
+        void SelectDllDir()
         {
-            if(_tvs == null)
+            string dir = EditorUtility.OpenFolderPanel("Dll文件目录", Application.dataPath, "");
+            if (string.IsNullOrEmpty(dir))
             {
-                _tvs = new TreeViewState();
-            }
-
-            _latv = new LinkAssemblyTreeView(_tvs);
-            _searchField = new SearchField();
-            _searchField.downOrUpArrowKeyPressed += _latv.SetFocusAndEnsureSelectedItem;
-        }
-
-        Vector2 _pos = Vector2.zero;
-
-        private void OnGUI()
-        {
-            GUILayout.BeginVertical();
-
-            if (GUILayout.Button("选择DLL所在目录", GUILayout.Height(30)))
-            {
-                string dir = EditorUtility.OpenFolderPanel("Dll文件目录", Application.dataPath, "");
-                if("" == dir)
-                {
-                    return;
-                }
-                _selectDir = dir;
-                _cmd = new CreateLinkXMLCommand(dir);
-                _cmd.onCreated += OnCreated;
-                _cmd.Excute();
-            }
-           
-            if (null != _nodeList)
-            {
-                _pos = GUILayout.BeginScrollView(_pos);
-
-                DoToolbar();
-                DoTreeView();
-                DoSaveView();
-
-                GUILayout.EndScrollView();
-            }
-
-            GUILayout.EndVertical();
+                return;
+            }            
+            _cmd = new CreateLinkXMLCommand(dir);
+            _cmd.onCreated += OnCreated;
+            _cmd.Excute();
         }
 
         private void OnCreated(CreateLinkXMLCommand cmd)
         {
-            _nodeList = _cmd.NodeList;
-            _cmd.onCreated -= OnCreated;
-            _latv.UpdateData(_nodeList);
+            nodeList = _cmd.NodeList;
+            _cmd.onCreated -= OnCreated;            
         }
 
-        void DoToolbar()
+        [LabelText("导出 [link.xml]"), Button(size: ButtonSizes.Medium)]
+        void CreateLinkXML()
         {
-            GUILayout.BeginHorizontal(EditorStyles.toolbar);
-            GUILayout.Label("search",GUILayout.Width(50));
-            //GUILayout.Space(100);
-            //GUILayout.FlexibleSpace();
-            _latv.searchString = _searchField.OnToolbarGUI(_latv.searchString);
-            GUILayout.EndHorizontal();
-        }
-
-        void DoTreeView()
-        {
-            Rect rect = GUILayoutUtility.GetRect(0, 100000, 0, 100000);
-            _latv.OnGUI(rect);
-        }
-
-        void DoSaveView()
-        {
-            if (GUILayout.Button("保存 link.xml", GUILayout.Height(30)))
+            string saveDir = EditorUtility.OpenFolderPanel("选择保存位置", Application.dataPath, "");
+            if (string.IsNullOrEmpty(saveDir))
             {
-                string saveDir = EditorUtility.OpenFolderPanel("选择保存位置", Application.dataPath, "");
-                if ("" == saveDir)
-                {
-                    return;
-                }
-                var savePath = FileSystem.CombinePaths(saveDir, "link.xml");
-                File.WriteAllText(savePath, _cmd.LinkXMLString);
-                //打开目录
-                ZeroEditorUtil.OpenDirectory(saveDir);
-                Debug.Log("创建成功");
+                return;
             }
-        }
+            var savePath = FileSystem.CombinePaths(saveDir, "link.xml");
+            File.WriteAllText(savePath, _cmd.LinkXMLString);
+            //打开目录
+            ZeroEditorUtil.OpenDirectory(saveDir);
+            ShowNotification(new GUIContent("导出完毕!"));            
+        }     
     }
 }
