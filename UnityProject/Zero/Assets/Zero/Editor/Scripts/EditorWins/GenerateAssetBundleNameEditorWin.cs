56f38b6194f274aa78d20cf4f46d2d6457375c17
Jing <122300890@qq.com>
2019-10-17 23:55:33 +0800

    update

--- UnityProject/Zero/Assets/Zero/Editor/Scripts/EditorWins/GenerateAssetBundleNameEditorWin.cs
+++ UnityProject/Zero/Assets/Zero/Editor/Scripts/EditorWins/GenerateAssetBundleNameEditorWin.cs
@@ -17,7 +17,14 @@ namespace Zero.Edit
     {        
         public const string TEMPLATE_FILE = "Assets/Zero/Editor/Configs/AssetBundleNameClassTemplate.txt";
 
-        public const string OUTPUT_CLASS_FILE = "Assets/@Scripts/Generated/AssetBundleName.cs";
+        public const string OUTPUT_CLASS_FILE = "Assets/@Scripts/Generated/AssetBundleName1.cs";
+
+        public const string FIELD_EXPLAIN_FORMAT = "\t\t" + @"/// <summary>
+        /// {0}
+        /// </summary>";
+
+        public const string FIELD_FORMAT = "\t\tpublic const string {0} = \"{1}.ab\";";
+
 
         /// <summary>
         /// 打开窗口
@@ -30,36 +37,89 @@ namespace Zero.Edit
             
         }
 
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            abList = FindAssetBundles(ZeroConst.HOT_RESOURCES_ROOT_DIR);
+        }
+        
+        [ListDrawerSettings(IsReadOnly =true)]
+        public List<AssetBundleItemVO> abList;
         
         public struct AssetBundleItemVO
         {
             /// <summary>
             /// 注释
             /// </summary>
+            [LabelText("添加注释")]
             public string explain;
 
             /// <summary>
             /// assetbundle名称
             /// </summary>
+            [LabelText("$GetFieldName")]
+            [DisplayAsString]
             public string assetbundle;
+
+            public string GetFieldName()
+            {
+                return assetbundle.Replace("/", "_").Replace(".ab","").ToUpper();
+            }
         }
 
         [LabelText("开始生成"), Button(size:ButtonSizes.Large)]
         void GeneratedAssetBundleNameClass()
         {
+            var dir = Directory.GetParent(OUTPUT_CLASS_FILE);
+            if (false == dir.Exists)
+            {
+                dir.Create();
+            }
+
             var template = File.ReadAllText(TEMPLATE_FILE);
             StringBuilder sb = new StringBuilder();
-            string fieldTemplate = "\t\tpublic const string {0} = \"{1}.ab\";";
-            sb.AppendLine(string.Format(fieldTemplate, "A", "a"));
-            sb.AppendLine();
-            sb.AppendLine(string.Format(fieldTemplate, "B", "b"));
-            sb.AppendLine();
-            sb.AppendLine(string.Format(fieldTemplate, "C", "c"));
+
+            foreach(var vo in abList)
+            {
+                if(false == string.IsNullOrEmpty(vo.explain))
+                {
+                    sb.AppendLine(string.Format(FIELD_EXPLAIN_FORMAT, vo.explain));
+                }                
+                sb.AppendLine(string.Format(FIELD_FORMAT, vo.GetFieldName(), vo.assetbundle));
+                sb.AppendLine();
+            }
+
             var classContent = template.Replace("{0}", sb.ToString());
             File.WriteAllText(OUTPUT_CLASS_FILE, classContent);
 
 
             UnityEditorInternal.InternalEditorUtility.OpenFileAtLineExternal(OUTPUT_CLASS_FILE, 0);
+        }
+
+        /// <summary>
+        /// 找出所有要打包的资源
+        /// </summary>
+        List<AssetBundleItemVO> FindAssetBundles(string resRootDir)
+        {
+            List<AssetBundleItemVO> list = new List<AssetBundleItemVO>();
+            string[] dirs = Directory.GetDirectories(resRootDir, "*", SearchOption.AllDirectories);
+            foreach(var dir in dirs)
+            {                
+                var di = new DirectoryInfo(dir);
+                if(di.GetFiles().Length == 0)
+                {
+                    continue;
+                }
+                
+                AssetImporter ai = AssetImporter.GetAtPath(dir);
+                string abName = ai.assetPath.Substring(resRootDir.Length + 1) + ZeroConst.AB_EXTENSION;
+
+                AssetBundleItemVO vo;
+                vo.explain = "";
+                vo.assetbundle = abName;
+                list.Add(vo);
+            }
+            return list;
         }
     }
 
